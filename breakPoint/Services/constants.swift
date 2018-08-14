a5261e40532b169a37066444d95c60560bf7f2e2
Stephen Reyes <sjreyes1@yahoo.com>
2018-08-14 14:05:45 -0700

    imported missing pd files/ set up add User ID function.

---
+++ breakPoint/Services/constants.swift
@@ -0,0 +1,13 @@
+//
+//  constants.swift
+//  breakPoint
+//
+//  Created by Stephen Reyes on 8/14/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+
+let DB_BASE = Database.database().reference()
