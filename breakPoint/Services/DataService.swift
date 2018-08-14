ab3b4e30a1c86fe47f0f87f04e906af9cfaa5263
Stephen Reyes <sjreyes1@yahoo.com>
2018-08-14 11:12:31 -0700

    project setup/connected fireBase framework.

---
+++ breakPoint/Services/DataService.swift
@@ -0,0 +1,16 @@
+//
+//  DataService.swift
+//  breakPoint
+//
+//  Created by Stephen Reyes on 8/14/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+}
